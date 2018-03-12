3c1736185cb50cb76864b5196b929f437861573f
do-sa <37302285+do-sa@users.noreply.github.com>
2018-03-12 18:27:40 +0300

    Add files via upload
    menu sayfası

---
+++ index.php
@@ -0,0 +1,10 @@
+<table border="1" align="center">
+<th>ANA MENU</th>
+<tr>
+	<td>
+		<a href='ogrencibilgi.php'>Ogrenci Bilgi</a><br>
+		<a href='derssecimi.php'>Ders Secimi</a><br>
+		<a href='bilgilerilistele.php'>Bilgileri Listele</a>
+	</td>
+</tr>
+</table>
